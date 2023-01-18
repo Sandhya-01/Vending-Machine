1d8c501936dc5b7dcee9daf35b9c78e2dc5c744e
Sandhya Mohan <sandhyamohanasundharam@outlook.com>
2023-01-18 21:18:37 +0530

    WorkingModel

--- Vending Machine/Program.cs
+++ Vending Machine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+//using Constraintset =  double;
 using System.Diagnostics.Metrics;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Configuration;
@@ -9,12 +10,27 @@ namespace VendingMachine
     public class Machine
     {
         static Machine customer = new Machine();
-        double currentamount;
-        public double balance;
-        int counter = 1;
+        double _currentamount;
+        double _balance;
+        
+        public double currentamount
+        {
+            get
+            {
+                return _currentamount;
+
+            }
+            set { _currentamount = Math.Round(value, 2); }
+        }
+        public double balance
+        {
+            get { return _balance; }
+            set { _balance = Math.Round(value, 2); }
+        }
+
         double coinvalue;
         double productamount;
-        string input="0";
+        string? input = "0";
         public Machine()
         {
             currentamount = 0;
@@ -23,16 +39,16 @@ namespace VendingMachine
         public void CalculateAmount(double amount)
         {
             customer.currentamount += amount;
-            Console.WriteLine("Current Balance :" + customer.currentamount);
+            //Console.WriteLine("Current Balance :" + customer.currentamount);
         }
-        public void ValidateInsertedCoin(string weight, string size, out double coinvalue)
+        public void ValidateInsertedCoin(string? weight, string? size, out double coinvalue)
         {
 
 
             if (weight == null || size == null || weight == "" || size == "")
             {
                 coinvalue = 0;
-                
+
 
             }
             else
@@ -59,18 +75,18 @@ namespace VendingMachine
                 else
                 {
                     coinvalue = 0;
-                    counter = 0;
+
                 }
-                
+
             }
             customer.DisplayValidMessage(customer.coinvalue);
         }
         public void DisplayValidMessage(double value)
         {
-            if (value == 0 || value == null)
+            if (value == 0)
             {
-                counter= 0;
-                Console.WriteLine("Invalid Coin.Please collect your coin");
+
+                Console.WriteLine("Invalid Coin! Please collect your coin");
                 Console.WriteLine("Current Balance :" + customer.currentamount);
 
             }
@@ -83,54 +99,55 @@ namespace VendingMachine
         public void DisplayProductDetails()
         {
             Console.WriteLine("Choose from below available Products");
-            Console.WriteLine("Press 1 for Cola $1");
-            Console.WriteLine("Press 2 for Chips $0.5");
-            Console.WriteLine("Press 3 for Candy $0.65");
-            
+            Console.WriteLine("Press 1 for Cola $" + VendingMachineConfig.ColaPrice);
+            Console.WriteLine("Press 2 for Chips $" + VendingMachineConfig.ChipsPrice);
+            Console.WriteLine("Press 3 for Candy $" + VendingMachineConfig.CandyPrice);
+
         }
         public void GetCoinDetails()
         {
-            Console.WriteLine("Please  Insert the Coin");
+
+            Console.WriteLine("**************WELCOME**************");
+            Console.WriteLine("Please Insert the Coin");
             Console.WriteLine("Note: input taken as weight of coin and size of the coin");
-            string weight = Console.ReadLine();
-            string size = Console.ReadLine();
+            var weight = Console.ReadLine();
+            var size = Console.ReadLine();
             customer.ValidateInsertedCoin(weight, size, out customer.coinvalue);
-            if(customer.currentamount!=0)
+            if (customer.currentamount != 0)
                 customer.PurchaseProduct();
         }
         public void PurchaseProduct()
         {
-            //if (customer.counter == 1)
-            //{
-                customer.DisplayProductDetails();
-                int choosenProduct = int.Parse(Console.ReadLine());
-                switch (choosenProduct)
-                {
-                    case 1:
-                        Console.Write("Choosen Product is Cola,Please Insert coins ");
-                        customer.balance = VendingMachineConfig.ColaPrice - customer.currentamount;
-                        customer.productamount = VendingMachineConfig.ColaPrice;
-                        break;
-                    case 2:
-                        Console.Write("Choosen Product is Chips,Please Insert coins ");
-                        customer.balance = VendingMachineConfig.ChipsPrice - customer.currentamount;
-                        customer.productamount = VendingMachineConfig.ChipsPrice;
-                        break;
-                    case 3:
-                        Console.Write("Choosen Product is Candy,Please Insert coins ");
-                        customer.balance = VendingMachineConfig.CandyPrice - customer.currentamount;
-                        customer.productamount = VendingMachineConfig.CandyPrice;
-                        break;
-                    default:
-                        Console.WriteLine("Enter Valid Product");
-                        customer.DisplayProductDetails();
-                        break;
-                }
-                Console.WriteLine("for the amount: " + customer.balance);
-                customer.ProcessingBill(customer.balance);
-            
+            customer.DisplayProductDetails();
+            var product = Console.ReadLine();
+            int choosenProduct = int.Parse(product == null ? "0" : product);
+            switch (choosenProduct)
+            {
+                case 1:
+                    Console.Write("Choosen Product is Cola ");
+                    customer.balance = VendingMachineConfig.ColaPrice - customer.currentamount;
+                    customer.productamount = VendingMachineConfig.ColaPrice;
+                    break;
+                case 2:
+                    Console.Write("Choosen Product is Chips ");
+                    customer.balance = VendingMachineConfig.ChipsPrice - customer.currentamount;
+                    customer.productamount = VendingMachineConfig.ChipsPrice;
+                    break;
+                case 3:
+                    Console.Write("Choosen Product is Candy ");
+                    customer.balance = VendingMachineConfig.CandyPrice - customer.currentamount;
+                    customer.productamount = VendingMachineConfig.CandyPrice;
+                    break;
+                default:
+                    Console.WriteLine("Enter Valid Product");
+                    customer.DisplayProductDetails();
+                    break;
+            }
+            Console.WriteLine("for the amount: " + customer.balance);
+            customer.ProcessingBill(customer.balance);
 
-        }       
+
+        }
         public static void Main()
         {
             var configurationBuilder = new ConfigurationBuilder();
@@ -138,55 +155,127 @@ namespace VendingMachine
             configurationBuilder.AddJsonFile(configpath, true);
             var root = configurationBuilder.Build();
             VendingMachineConfig appconfig = root.GetSection("VendingMachineConfig").Get<VendingMachineConfig>();
-            while(customer.input!="1")
-                customer.GetCoinDetails();
-            while(customer.input=="1")
-                customer.PurchaseProduct();
-            
+            while (customer.input != "1")
+            {
+                if (customer.input == "2")
+                {
+                    customer.PurchaseProduct();
+
+                }
+                else if (customer.input == "3")
+                {
+                    Console.WriteLine("Please collect your balance");
+                    customer.currentamount = 0;
+                    customer.GetCoinDetails();
+                }
+                else if (customer.input == "0")
+                    customer.GetCoinDetails();
+                while (customer.input == "1")
+                    customer.PurchaseProduct();
+
+            }
+
         }
 
         public void ProcessingBill(double balance)
         {
-            while ( balance>0)
+            //bool flag = true;
             {
-                string weight = Console.ReadLine();
-                string size = Console.ReadLine();
-                customer.ValidateInsertedCoin(weight, size, out coinvalue);
-                
-                customer.balance = productamount - customer.currentamount;
-                balance = customer.balance;
-                if (customer.currentamount >= customer.productamount)
+                while (balance >0 )
                 {
-                    Console.WriteLine("Please collect your Product");
-                    customer.currentamount -= customer.productamount;
-                    if (customer.currentamount == 0)
+                    Console.WriteLine("Press 2 to cancel item and go back to menu");
+                    Console.WriteLine("Press 3 to cancel item and get the balance");
+                    Console.WriteLine("Press any other key to continue current purchase");
+                    int val = customer.CancelorChangeItem(int.Parse(Console.ReadLine()));
+                    if (val != 1) { break; }
+                    else if (val == 1)
                     {
-                        customer.input = "0";
-                        break;
-                    }
-                    else if (customer.currentamount > 0)
-                    {
-                        Console.WriteLine("Current balance is :" + customer.currentamount);
-                        Console.WriteLine("To Continue your shopping, Press 1");
-                        Console.WriteLine("To Exit and collect balance amount, Press any key except 1");
-                        customer.input = Console.ReadLine();
-                        if (customer.input != "1")
+                        Console.WriteLine("Please Insert coins for the amount $" + customer.balance);
+                        while (customer.input != "Enter")
                         {
-                            Console.WriteLine("Please collect your balance");
-                            break;
+                            var weight = Console.ReadLine();
+                            if (weight == "Enter")
+                            {
+                                customer.input = "Enter";
+                                //flag = false;
+                                break;
+                            }
+
+                            else
+                            {
+                                var size = Console.ReadLine();
+
+                                customer.ValidateInsertedCoin(weight, size, out coinvalue);
+                            }
                         }
+                        Console.WriteLine("Current Balance: $" + customer.currentamount);
+
+                        customer.balance = Math.Abs(productamount - customer.currentamount);
+                        balance = customer.balance;
+                        if (customer.currentamount >= customer.productamount)
+                        {
+                            Console.WriteLine("Please collect your Product");
+                            Console.WriteLine("**************THANK YOU**************");
+                            
+                            
+                            customer.currentamount -= customer.productamount;
+
+                                if (customer.currentamount == 0)
+                            {
+                                customer.input = "0";
+                                Thread.Sleep(3000);
+                                Console.Clear();
+                                break;
+                            }
+                            else if (customer.currentamount > 0)
+                            {
+                                Console.WriteLine("Current balance is :" + customer.currentamount);
+                                Console.WriteLine("To Continue your shopping, Press 1");
+                                Console.WriteLine("To Exit and collect balance amount, Press any key except 1");
+                                customer.input = Console.ReadLine();
+                                if (customer.input != "1")
+                                {
+                                    Console.WriteLine("Please collect your balance $"+customer.balance);
+                                    Thread.Sleep(3000);
+                                    Console.Clear();
+
+
+                                    break;
+                                }
+                                break;
+                            }
+
+
+                        }
+
+                        Console.WriteLine("Insert coins for :" + customer.balance);
+                        
                     }
-                   
                     
+
                 }
-                
-                Console.WriteLine("Insert coins for :" + (customer.balance )); 
-                 
-                
+
+
             }
+        }
+        public int CancelorChangeItem(int i)
+        {
+            if (i == 2)
+            {
+                customer.input = "2";
+                return 0;
+            }
+            else if (i == 3)
+            {
+                customer.input = "3";
+                return -1;
 
-            
-
+            }
+            else
+            {
+                customer.input = "1";
+                return 1;
+            }
         }
     }
 }
